88d8d69c63b0e8a1e7bcadc872195c2d922fd5dd
AthulGeorge117 <athulgeorge37@gmail.com>
2022-05-09 18:53:36 +1000

    fixes key errors and creates walls on click event

--- src/Grid.js
+++ src/Grid.js
@@ -1,4 +1,5 @@
 import Grid_Cell from "./Grid_Cell"
+import { useState } from 'react';
 import "./Grid.css"
 
 const GRID_HEIGHT = 20
@@ -6,32 +7,32 @@ const GRID_LENGTH = 40
 
 function Grid() {
 
+    const [my_Grid, setGrid] = useState([]);
 
-    const my_grid = []
     
     for (let y = 0; y < GRID_HEIGHT; y++) {
         const my_row = []
         for (let x = 0; x < GRID_LENGTH; x++) {
-            my_row.push([])
+            my_row.push([x.toString() + " " + y.toString])
         }
-        my_grid.push(my_row)
+        my_Grid.push(my_row)
     }
 
 
 
     return (
         <div className="Grid">
-            {my_grid.map((row) => {
+            {my_Grid.map((row, row_ID) => {
                 return (
-                    <div className="Grid_Row">
-                        {row.map((col) => {
-                        return <Grid_Cell/>;
+                    <div className="Grid_Row" key={row_ID}>
+                        {row.map((col, col_ID) => {
+                        return <Grid_Cell key={col_ID} x={col_ID} y={row_ID}/>;
                         })}
                     </div>
                 );
             })}
         </div>
-    )
+    );
 }
 
 export default Grid
--- src/Grid_Cell.js
+++ src/Grid_Cell.js
@@ -1,25 +1,26 @@
 import { useState } from 'react';
 import './Grid_Cell.css';
 
-function Grid_Cell() {
+function Grid_Cell(props) {
 
-    const [wall, setWall] = useState(false)
+    const [Wall, setWall] = useState(false);
 
-    // function setWall() {
-    //     return true
-    // }
-
-
-
-    const Grid_Cell_class_name = "Grid_Cell"
-
-    if (wall) {
-        Grid_Cell_class_name += " Wall"
+    const Invert_Wall = () => {
+        setWall(!Wall);
     }
 
+
+    let Grid_Cell_class_name = "Grid_Cell";
+
+    if (Wall) {
+        Grid_Cell_class_name += " Wall";
+    }
+
+
     return (
-        <div className={Grid_Cell_class_name}></div>
+        <div className={Grid_Cell_class_name} onClick={Invert_Wall}>
+        </div>
     )
 }
 
-export default Grid_Cell
+export default Grid_Cell;
