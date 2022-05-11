81cf549f8c4cbe0279da929633119a2f3a67befe
AthulGeorge117 <athulgeorge37@gmail.com>
2022-05-12 03:04:30 +1000

    better animation for visited cells

--- src/Grid.js
+++ src/Grid.js
@@ -1,5 +1,5 @@
 import Grid_Cell from "./Grid_Cell"
-import { useState, } from 'react';
+import { useEffect, useState, } from 'react';
 import "./Grid.css"
 import { PriorityQueue } from "./priority_queue"
 
@@ -12,18 +12,16 @@ const START_CELL_Y = 10
 const END_CELL_X = 30
 const END_CELL_Y = 15
 
-const DELAY_ANIMATION = 5
+const DELAY_ANIMATION = 10   // was 5
 
-function Grid() {
-
-    // const my_Grid = []
-    const [my_Grid, set_my_Grid] = useState([])
+let my_start_cell = null
+let my_end_cell = null
 
 
-    const visited_cells = []
+const initialise_empty_grid = () => {
 
-    let my_start_cell = null
-    let my_end_cell = null
+    const empty_grid = []
+
     for (let y = 0; y < GRID_HEIGHT; y++) {
         const my_row = []
         for (let x = 0; x < GRID_LENGTH; x++) {
@@ -56,12 +54,71 @@ function Grid() {
             }
 
         }
-        my_Grid.push(my_row)
+        empty_grid.push(my_row)
+    }
+
+    is_empty_grid(empty_grid)
+    return empty_grid
+}
+
+
+const is_empty_grid = (my_Grid) => {
+    let is_empty = true
+    for (const row of my_Grid) {
+        for (const my_cell of row) {
+            console.log("my_cell.is_Wall", my_cell.is_Wall)
+            if (my_cell.is_Wall || my_cell.is_path) {
+                is_empty = false
+            }
+            
+        }
+    }
+
+    console.log()
+    if (is_empty) {
+        console.log("my_Grid is empty")
+    } else {
+        console.log("my_Grid is not empty!!!")
+    }
+    console.log()
+}
+
+
+
+function Grid() {
+    // the grid is only rendered once, grid cell components are updated and rendered for walls
+    // searched areas and paths are updated using getElementByID classnames
+
+    // all grid cell objects are kept in this array
+    // const my_Grid = []
+    const [my_Grid, set_my_Grid] = useState(initialise_empty_grid())
+
+    // contains all visited cell objects
+    const visited_cells = []
+
+    // initialising the grid cells
+
+
+    useEffect(() => {
+        console.log()
+        console.log("useffect running")
+
+        is_empty_grid(my_Grid)
+        console.log()
+
+    });
+
+    const empty_grid = () => {
+
+        console.log()
+        console.log("clearing grid")
+        set_my_Grid(initialise_empty_grid())
+        console.log()
     }
 
 
     let mouse_down = false
-
+    // below 3 handle_mouse methods are for clicking and dragging walls on the grid
     const handle_mouse_down = (update_wall, x, y) => {        
         update_wall()
         my_Grid[y][x].is_Wall = !my_Grid[y][x].is_Wall
@@ -81,17 +138,17 @@ function Grid() {
 
 
     const A_STAR_Algorithm = () => {
-        console.log("A Star algo")
-        // console.log("my_Grid =", my_Grid)
+        console.log("Starting A Star algo")
 
         let my_queue = new PriorityQueue();
 
         my_queue.enqueue(my_start_cell, 0)
 
-        let came_from = {}
+        let came_from = {} // a dictionary of values containing where each cell originated from of type(cell)
         came_from[my_start_cell.my_key] = null
 
-        let cost_so_far = {}
+        let cost_so_far = {} // a dictionary containing the cell as the key 
+        // and the distance from start cell to the current cell as the value
         cost_so_far[my_start_cell.my_key] = 0
 
         let current_cell = null
@@ -99,15 +156,17 @@ function Grid() {
             current_cell = my_queue.dequeue().element
         
             if (current_cell === my_end_cell) {
+                // end cell found, breaking out of while loop
                 break
             }
 
-            const neighbor_cells = generate_neighbor_cells(my_Grid, current_cell)
-            for (let n=0; n < neighbor_cells.length; n++) {
+            // going through each of the valid neighbor cells 
+            // and adding them to the dictionaries
+            for (const neighbor_cell of generate_neighbor_cells(current_cell)) {
 
-                const neighbor_cell = neighbor_cells[n]
                 const new_cost = cost_so_far[current_cell.my_key] + 1
 
+                // adding each neighbor cell to visited cell to animate them later
                 visited_cells.push(neighbor_cell)
 
                 if (!(neighbor_cell.my_key in cost_so_far) || new_cost < cost_so_far[neighbor_cell.my_key]) {
@@ -120,12 +179,10 @@ function Grid() {
             }
         }
 
-        
+        // constructing the path
         const my_cell_path = []
         let my_cell = current_cell
-        let n = 1;
         while (my_cell != null) {
-
             my_cell.is_path = true      
             my_cell_path.push(my_cell)
             my_cell = came_from[my_cell.my_key]
@@ -134,9 +191,9 @@ function Grid() {
         // checking if we have found a path
         if (my_cell_path[0] === my_end_cell) {
             console.log("end cell found")
-            console.log("my_cell_path", my_cell_path)
+            // console.log("my_cell_path", my_cell_path)
 
-            // constructing the searched area with a delay
+            // changing cells to searched area color with a delay
             for (let k=0; k < visited_cells.length; k++) {
                 setTimeout(() => {
                     const neighbor_cell = visited_cells[k]
@@ -149,13 +206,12 @@ function Grid() {
                         visited_class_name += " End";
                     }
 
-
                     document.getElementById(neighbor_cell.my_key).className = visited_class_name
-                }, DELAY_ANIMATION * k)
+
+                }, DELAY_ANIMATION * k);
             }
 
-
-            // creating the path with a delay
+            // changing cells to path color with a delay
             const do_after = visited_cells.length * DELAY_ANIMATION
             for (let n=0; n < my_cell_path.length; n++) {
                 setTimeout(() => {
@@ -169,36 +225,39 @@ function Grid() {
                         path_class_name += " End";
                     }
 
-
                     document.getElementById(my_cell.my_key).className = path_class_name
-                }, do_after + (DELAY_ANIMATION * n))
+
+                }, do_after + ((DELAY_ANIMATION + 25) * n));
             }
 
-            // set_my_Grid(my_Grid)
         } else {
             console.log("could not find the end cell")
         }
     }
 
 
-    const generate_neighbor_cells = (grid, current_cell) => {
+    const generate_neighbor_cells = (current_cell) => {
+        // generates all valid neighbor cells of a particular cell
+
         //                       x, y
         const neighbor_index = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
         const my_neighbors = []
-        for (let i=0; i < 4; i++) {
+        for (let i=0; i < neighbor_index.length; i++) {
             try {
                 const new_x = current_cell.x_val + neighbor_index[i][0]
                 const new_y = current_cell.y_val + neighbor_index[i][1]
 
                 const new_neighbor = my_Grid[new_y][new_x]
 
+                // checking if the neighbor cell is a wall or not
                 if (!new_neighbor.is_Wall) {
                     my_neighbors.push(new_neighbor)
                 }
                 
             }
             catch (TypeError) {
+                // incase we run into the edges of the grid, we can avoid index errors
                 continue
             }
         }
@@ -208,6 +267,7 @@ function Grid() {
 
 
     const heuristic = (cell) => {
+        // estimates the L distance from a particalar cell to the end cell
         return Math.abs(my_end_cell.x_val - cell.x_val) + Math.abs(my_end_cell.y_val - cell.y_val)
     }
 
@@ -215,6 +275,7 @@ function Grid() {
     return (
         <>
             <button onClick={A_STAR_Algorithm}>Start A*</button>
+            <button onClick={empty_grid}>Clear Grid</button>
             <div className="Grid">
                 {my_Grid.map((row, row_ID) => {
                     return (
--- src/Grid_Cell.js
+++ src/Grid_Cell.js
@@ -5,14 +5,15 @@ function Grid_Cell (props) {
 
     const [Wall, setWall] = useState(props.is_Wall)
 
+    // Invert_Wall is called from the Grid component to work for click and drag events
     const Invert_Wall = () => {
         if (!props.is_Start && !props.is_End) {
-            console.log("changing wall at", props.my_key, "from ", Wall, "to", !Wall)
+            // console.log("changing wall at", props.my_key, "from ", Wall, "to", !Wall)
             setWall(!Wall)
         }
     }
 
-
+    // needed to initialise each grid cell as either a wall, start cell or end cell
     let Grid_Cell_class_name = "Grid_Cell " + props.my_key;
 
     if (Wall) {
@@ -23,7 +24,10 @@ function Grid_Cell (props) {
         Grid_Cell_class_name += " End"
     }
 
+    // console.log(Grid_Cell_class_name)
 
+    // mouse events are sent back to the Grid component
+    // so that multiple cell walls can be updated on click and drage events
     return (
         <div 
         id={props.my_key}
